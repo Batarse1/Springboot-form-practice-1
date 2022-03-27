05bc30a8d630f72e2d033f1dc283907723576273
batarse1 <00014419@uca.edu.sv>
2022-03-26 18:36:09 -0600

    :art: Format util

--- spring-boot-form/src/main/java/com/uca/spring/util/Util.java
+++ spring-boot-form/src/main/java/com/uca/spring/util/Util.java
@@ -4,17 +4,18 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-
 public class Util {
   private static final String DATE_FORMAT = "dd/MM/yyyy";
 
   public static boolean isDateValid(String strDate) {
-    
-    if(strDate==null || strDate.isEmpty())return false;
-    
+
+    if (strDate == null || strDate.isEmpty()){
+      return false;
+    }
+
     DateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
     sdf.setLenient(false);
-    
+
     try {
       sdf.parse(strDate);
     } catch (ParseException e) {
@@ -22,6 +23,5 @@ public class Util {
     }
     return true;
   }
-
 
 }
